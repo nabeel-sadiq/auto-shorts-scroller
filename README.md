ea5b32d942e2e900c9ff5ba113d9f6c0bde8597d
nabeel-sadiq <nabeelsadiq.dev@gmail.com>
2025-03-15 17:27:35 +0500

    done a lot

---
+++ README.md
@@ -0,0 +1,3 @@
+# YouTube Shorts Auto Scroller
+Firefox extension that automatically scrolls shorts when the current one finishes.
+Just install it and it automatically applies the necessary javascript for auto-scrolling without any configuration.
--- src/main.js
+++ src/main.js
@@ -2,21 +2,22 @@ function swipeShorts() {
   try {
     const video = document.querySelector("video.video-stream");
     const NEXT_VIDEO_BUTTON_SELECTOR = "#navigation-button-down > ytd-button-renderer > yt-button-shape > button";
-    const sponsored_selector = ".badge-shape-wiz__text"
-    const sponsored_elem = document.querySelector(sponsored_selector);
     const nextBtn = document.querySelector(NEXT_VIDEO_BUTTON_SELECTOR);
+    let hasClicked = false;
 
     if (!nextBtn) throw new Error("Next Button not found!");
     if (!video) throw new Error("Video not found!");
-    if (sponsored_elem?.textContent == "Sponsored") console.log(sponsored);
-    
     console.log(nextBtn)
+    console.log(video);
 
     video.addEventListener("timeupdate", () => {
-      const buffer = 0.2; // Small buffer to ensure it doesn't miss the end
-      if (video.currentTime >= video.duration - buffer) {
-        console.log("boutta scroll")
-        nextBtn.click();
+      const buffer = 0.5; // Small buffer to ensure it doesn't miss the end
+      if (video.currentTime >= video.duration - buffer && !hasClicked) {
+        hasClicked = true;
+        setTimeout(() => {
+          nextBtn.click();
+          hasClicked = false;
+        }, 500);
       }
     });
   } catch (error) {
@@ -24,4 +25,5 @@ function swipeShorts() {
   }
 }
 
+console.log("yes")
 swipeShorts();
